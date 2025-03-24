ff1df26b0a19cf8f2a826bed1d214dc40a6e322e
Juan Orlando <juan.alves@fatec.sp.gov.br>
2025-03-24 20:47:39 -0300

    Inicio

---
+++ App.js
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/home/HomeScreen';
+
+const Tab = createBottomTabNavigator();export default function App() {
+return (
+<NavigationContainer>
+<Tab.Navigator>
+<Tab.Screen name="Home" component={HomeScreen} />
+</Tab.Navigator>
+</NavigationContainer>
+);
+}
