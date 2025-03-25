e98c2240518cfface451ac128099c391571dc9b9
Juan Orlando <juan.alves@fatec.sp.gov.br>
2025-03-24 21:13:06 -0300

    Mudança no app.js para drawer

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/home/HomeScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Tab = createBottomTabNavigator();export default function App() {
-return (
-<NavigationContainer>
-<Tab.Navigator>
-<Tab.Screen name="Home" component={HomeScreen} />
-</Tab.Navigator>
-</NavigationContainer>
-);
-}
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+    return (
+    <NavigationContainer>
+    <Drawer.Navigator initialRouteName="Home"><Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
+    </NavigationContainer>
+    );
+    }
+
+   //  <Drawer.Screen name="Tarefas" component={TarefaScreen} />
+
+   // <Drawer.Screen name="Cronômetro" component={CronometroScreen} />
+   
